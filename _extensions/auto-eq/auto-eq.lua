d71f4b4deef03494ae21c99a5d2969edfb8c0980
Sean Marshallsay <smarshallsay01@qub.ac.uk>
2026-02-28 17:30:17 +0000

    Fix extension for display math embedded in paragraphs.
    Originally extension assumed that a display math equation would be in a
    paragraph on its own, meaning it didn't work when an equation was embedded
    in a paragraph. This change updates the extension to look through the inlines
    of a paragraph and add a reference to each display math equation found.

--- _extensions/auto-eq/auto-eq.lua
+++ _extensions/auto-eq/auto-eq.lua
@@ -1,57 +1,53 @@
-
---print("Starting auto-eq.lua")
-
-function isDisplayMath(el)
-  return el.t == "Math" and el.mathtype == "DisplayMath"
+local function isDisplayMath(el)
+  return el ~= nil and el.t == "Math" and el.mathtype == "DisplayMath"
 end
 
+local function isEqRef(el)
+  return el ~= nil and el.t == "Str" and el.text:match("^{#eq-")
+end
 
-function hasEq(el)
-  
-  for i, e in ipairs(el) do
-    if e.t == "Str" then
-      return true
+local function isNoEqRef(el)
+  return el ~= nil and el.t == "Str" and el.text == "{#no-eq}"
+end
+
+local function findNextNonSpace(inlines, start)
+    local i = start + 1
+    -- TODO: There are probably other whitespace elements that need added here.
+    while (i <= #inlines and (inlines[i].t == "Space" or inlines[i].t == "SoftBreak")) do
+        i = i + 1
     end
-  end
 
-  return false
-  
+    return i
 end
 
-function removeNoEq(el)
-  
-  for i, e in ipairs(el) do
-    if e.t == "Str" and e.text == "{#no-eq}" then
-      el.remove(el, i)
-    end
-  end
-  
-  return el
-end
-    
 unnameCounter = 0
--- adapting from https://github.com/quarto-dev/quarto-cli/blob/56da834f07f5fdfab1e432f11aa3be6b26f4fd2a/src/resources/filters/crossref/equations.lua
 
--- ^ they also have a function set for Plain group as well
---   Maybe I should add this function for this group too?
+-- TODO: Original (https://github.com/quarto-dev/quarto-cli/blob/56da834f07f5fdfab1e432f11aa3be6b26f4fd2a/src/resources/filters/crossref/equations.lua) has a function for `Plain` as well, should we add for this?
 function Para(element)
-  
   local inlines = element.content
-  
+
   if inlines:find_if(isDisplayMath) == nil then
     return element
   end
-  
-  --print(element)
-  
-  if not hasEq(inlines) then
-    inlines:insert(pandoc.Str("{#eq-unnamed-" .. unnameCounter .. "}"))
-    unnameCounter = unnameCounter + 1
-    return element
-  else
-    removeNoEq(inlines)
+
+  local i = 1
+  while (i <= #inlines) do
+    local math = inlines[i]
+
+    local j = findNextNonSpace(inlines, i)
+    local ref = inlines[j]
+
+    if isDisplayMath(math) then
+      if isNoEqRef(ref) then
+        inlines:remove(i + 1)
+      elseif not isEqRef(ref) then
+        inlines:insert(i + 1, pandoc.Str("{#eq-unnamed-" .. unnameCounter .. "}"))
+        unnameCounter = unnameCounter + 1
+      end
+    end
+
+    i = i + 1
   end
-  
-  
+
   return element
 end
